f0b0285ff1195c4fb69a812321793f83fb16b21e
HP <HP@LAPTOP-9U6BEHI8>
2025-09-29 09:42:54 +0530

    sample java code1

---
+++ corejavaprogram/src/corejavadaya1/sample.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class student {
+
+	 private int id;
+		 private string name;
+		 private string age;
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		this.name=name;
+
+	}
+
+}
